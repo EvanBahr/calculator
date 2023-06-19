ddc241f6849d55f0ca9960d2c5519ca8903ce5d0
EvanBahr <127106593+EvanBahr@users.noreply.github.com>
2023-06-19 11:19:39 +0700

    Update home.jsx

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Calculator from "../components/Calculator/Calculator";
-
+import Footer from "../components/Calculator/Footer";
 const Home = () => {
   return (
     <div>
       <Calculator />
+    <Footer />
     </div>
   );
 };
